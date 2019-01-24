0c86e6ee5a914e5f15485b5531a550cd40bebbc3
Jessica Gladstone <jcg321@comcast.net>
2019-01-24 17:45:53 +0000

    Automatically backed up by Learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -19,5 +19,5 @@ def select_series_title_with_most_human_characters
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "SELECT name, COUNT(*) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY character_books.character_id ORDER BY COUNT(*) DESC, characters.name;"
+  "SELECT character.name, COUNT(*) FROM characters JOIN character_books ON characters.id = character_books.character_id GROUP BY character_books.character_id ORDER BY COUNT(*) DESC, characters.name;"
 end
